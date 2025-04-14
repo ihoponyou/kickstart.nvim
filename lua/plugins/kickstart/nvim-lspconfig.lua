af87b52adfb36a5000bd0ea7cbf8dbeeecdd0ca4
noahmagcalas <noah.magcalas@gmail.com>
2025-04-14 17:39:37 -0500

    feat: w omnisharp

--- lua/plugins/kickstart/nvim-lspconfig.lua
+++ lua/plugins/kickstart/nvim-lspconfig.lua
@@ -181,7 +181,7 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- omnisharp = {},
+      omnisharp = {},
       clangd = {
         cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose' },
       },
